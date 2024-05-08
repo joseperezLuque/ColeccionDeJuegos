1c8cc0891053b1d671e8c97f8bd59333ab57bfc0
joseperezLuque <jose.perez.l@tecsup.edu.pe>
2024-05-08 11:40:30 -0500

    Initial Commit

---
+++ PerezColeccionDeJuegos/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PerezColeccionDeJuegos
+//
+//  Created by Jose Adriano Perez Luque on 8/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
